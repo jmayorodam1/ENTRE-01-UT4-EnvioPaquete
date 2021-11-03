9bdaa1726b347677545a830a3eabd8c1e6d05712
Javier Mayor Oyarzun <jmayorodam1@gmail.com>
2021-11-03 19:58:26 +0100

    testEnvio

---
+++ TestEnvio.java
@@ -0,0 +1,32 @@
+
+/**
+ * Clase de prueba
+ * 
+ * @author- Javier Mayor 
+ */
+public class TestEnvio
+{
+    
+
+    /**
+     * Constructor for objects of class TestEnvio
+     */
+    public TestEnvio()
+    {
+        Paquete paquete1 = new Paquete();
+        Paquete paquete2 = new Paquete(40,60,40);
+        Envio envi = new Envio();
+        envi.addPaquete(paquete1);
+        envi.addPaquete(paquete2);
+        envi.print();
+        
+        Paquete paquete3 = new Paquete(new Dimension(50,50,70),13);
+        envi.addPaquete(paquete1);
+        envi.print();
+        envi.addPaquete(paquete2.obtenerCopia());
+        
+        
+    }
+
+    
+}

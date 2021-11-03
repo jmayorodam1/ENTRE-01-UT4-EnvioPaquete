1bbe73fb5df25ec4c98ba89a873332ddaac02c4b
Javier Mayor Oyarzun <jmayorodam1@gmail.com>
2021-11-03 19:36:50 +0100

    Envio getNumeroPaquetes envioCompleto addPaquete

--- Envio.java
+++ Envio.java
@@ -55,8 +55,17 @@ public class Envio
      * (dependerá de cuántos paquetes estén a null)
      */
     public int getNumeroPaquetes() {
-        //TODO
-       return 0;
+        int paquetes = 0;
+        if(paquete1 == null){
+            paquetes++;
+        }
+        if(paquete2 == null){
+            paquetes++;
+        }
+        if(paquete3 == null){
+            paquetes++;
+        }
+        return paquetes;
 
     }
 
@@ -65,8 +74,8 @@ public class Envio
      * (tiene exactamente 3 paquetes)
      */
     public boolean envioCompleto() {
-       //TODO
-       return false;
+
+        return getNumeroPaquetes() == 3;
 
     }
 
@@ -78,11 +87,24 @@ public class Envio
      * si se añade como primero, segundo o tercero (no han de quedar huecos)
      */
     public void addPaquete(Paquete paquete) {
-       //TODO
+        if(getNumeroPaquetes() == 0){
+
+            paquete1 = paquete;
+        }
+        else if(getNumeroPaquetes() == 1){
+
+            paquete2 = paquete;
+        }
+        else if(getNumeroPaquetes() == 2){
+
+            paquete3 = paquete;
+        }
+        else{
         
+            System.out.println("No se admiten mas paqutes en el envio");        
+        }
 
     }
-
     /**
      * Calcula y devuelve el coste total del envío
      * 
@@ -96,7 +118,7 @@ public class Envio
      */
     public double calcularCosteTotalEnvio() {
         //TODO
-       return 0;
+        return 0;
 
     }
 
@@ -106,8 +128,8 @@ public class Envio
      * (leer enunciado)
      */
     public String toString() {
-       //TODO
-       return null;
+        //TODO
+        return null;
     }
 
     /**
@@ -119,5 +141,4 @@ public class Envio
         System.out.println(this.toString());
     }
 
-    
 }
